935dfa6a8381044581b77ea9a568b502a9344d1a
Aromie <43172745+Aromie@users.noreply.github.com>
2022-09-21 23:56:32 +0900

    Create natural_words_deeplearning_ex_proj2_0.py

---
+++ natural_words_deeplearning_ex_proj2_0.py
@@ -0,0 +1,18 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt # 시각화
+import re # regex 특수문자, 특정 키워드 일괄 처리
+import urllib.request
+from konlpy.tag import Okt # 한국어 토크나이저
+from tensorflow.keras.preprocessing.text import Tokenizer # 우리가 실습했던 내용들 라이브러리로 사용예정
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+
+urllib.request.urlretrieve("http://raw.githubusercontent.com/e9t/nsmc/master/ratings_train.txt", filename="rating_train.txt")
+urllib.request.urlretrieve("http://raw.githubusercontent.com/e9t/nsmc/master/ratings_test.txt", filename="ratings_test.txt")
+
+train_data= pd.read_table('/content/rating_train.txt')
+test_data= pd.read_table('/content/ratings_test.txt')
+
+print(train_data[:5])
+
+
